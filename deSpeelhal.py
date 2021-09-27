a206aa9d9ab8b71be937f0580a0823805ad8556f
Lars van den Nieuwendijk <larsvdnieuwendijk@t-mobilethuis.nl>
2021-09-27 11:37:35 +0200

    Nu wel gecommit lol

---
+++ deSpeelhal.py
@@ -0,0 +1,9 @@
+# toegangsTicket = 7.45
+# aantalMinutenVR = 45
+# gameTijd = 0.37 * 9
+# personen = 3
+# totaal = toegangsTicket + gameTijd * personen
+#
+# print(str(toegangsTicket + gameTijd * personen))
+#
+# print(("Dit geweldige dagje-uit met ") + str(personen) + (" mensen in de Speelhal met 45 minuten VR kost je maar ") + str(totaal) + (" euro "))
---
+++ feestLunch.py
@@ -0,0 +1,17 @@
+# aantalCroissants = 17
+# croissants = aantalCroissants * 0.39
+# aantalStokbroden = 2
+# stokbroden = aantalStokbroden * 2.78
+# aantalKortingsbonnen = 3
+# kortingsBonnen = aantalKortingsbonnen * 0.50
+# totaal = croissants + stokbroden - kortingsBonnen
+#
+# # print(str(totaal))
+#
+# print(("De feestlunch kost je bij de bakker ") + str(totaal) + (" euro voor de ") + str(aantalCroissants) + (" croissantjes en de ") + str(aantalStokbroden) + (" stokbroden als de ") + str(aantalKortingsbonnen) + (" kortingsbonnen nog geldig zijn!")) # YAY! messy code
+
+aantalCroissants = int(input('Hoeveel croissants? '))
+prijsCroissants = float(input('Prijs van croissants? ' ))
+aantalStokbroden = int(input('Hoeveel stokbroden? '))
+prijsStokbroden = float(input('Prijs van stokbroden? '))
+aantalKortingsbonnen = int(input('Hoeveel kortingsbonnen? '))
